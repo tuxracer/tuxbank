9d6753d4adf305dff9a5ee6bf72c90464ef4fd88
Derek Petersen <tuxracer@gmail.com>
2026-06-03 19:52:27 -0700

    build: remove Next.js; vite scripts, typescript-eslint config, tsconfig cleanup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,19 +1,27 @@
 import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import jsxA11y from "eslint-plugin-jsx-a11y";
+import globals from "globals";
 
 const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "dist/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
+  globalIgnores(["dist/**"]),
+  js.configs.recommended,
+  tseslint.configs.recommended,
+  jsxA11y.flatConfigs.recommended,
+  reactHooks.configs.flat["recommended-latest"],
+  {
+    plugins: { "react-refresh": reactRefresh },
+    languageOptions: { globals: { ...globals.browser } },
+    rules: {
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true },
+      ],
+    },
+  },
 ]);
 
 export default eslintConfig;
